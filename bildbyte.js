d8ceaa62124aaa0a3a182d5d27edc722b4327c8e
Andreas <nojdhish@gmail.com>
2020-01-28 04:45:16 +0100

    first commit

---
+++ bildbyte.js
@@ -0,0 +1,17 @@
+function bildbyte(input) {
+    if (input == 1) {
+      document.getElementById('imageshower').src = 'customs.png';
+    }
+    else if (input == 2){
+      document.getElementById('imageshower').src = 'factory.jpg';
+    }
+    else if (input == 3){
+      document.getElementById('imageshower').src = 'thelab.png';
+    }
+    else if (input == 4){
+      document.getElementById('imageshower').src = 'interchange.jpg';
+    }
+    else if (input == 5){
+        document.getElementById('imageshower').src = 'woods.jpg';
+      }
+  }
