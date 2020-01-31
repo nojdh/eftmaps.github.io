7f1324d887d64cb5b0bb8a4d1f3c3ca55fa082a1
Andreas <nojdhish@gmail.com>
2020-01-31 03:16:49 +0100

    leafletjs

--- bildbyte.js
+++ bildbyte.js
@@ -1,23 +1,28 @@
 function bildbyte(input) {
-    if (input == 1) {
-      document.getElementById('imageshower').src = 'customs.png';
-    }
-    else if (input == 2){
-      document.getElementById('imageshower').src = 'factory.jpg';
-    }
-    else if (input == 3){
-      document.getElementById('imageshower').src = 'thelab.png';
-    }
-    else if (input == 4){
-      document.getElementById('imageshower').src = 'interchange.jpg';
-    }
-    else if (input == 5){
-        document.getElementById('imageshower').src = 'woods.jpg';
-    }
-    else if (input == 6){
-      document.getElementById('imageshower').src = 'factory3d.jpg';
-    }
+  if (input == 1) {
+    bytbild('customs.png', 5, 9)
+  }
+  else if (input == 2){
+    bytbild('factory.jpg', 5, 9)
+  }
+  else if (input == 3){
+    bytbild('thelab.png', 5, 9)
+  }
+  else if (input == 4){
+    bytbild('interchange.jpg', 5, 9)
+  }
+  else if (input == 5){
+    bytbild('woods.jpg', 5, 9)
+  }
+  else if (input == 6){
+    bytbild('factory3d.jpg', 5, 9)
+  }
   else if (input == 7){
-    document.getElementById('imageshower').src = 'reserve.png';
+  bytbild('reserve.png', 5, 9)
   }
-  }
+}
+
+function bytbild(bild, x, y) {
+  var bounds = [[0,0], [x, y ]];
+  image = L.imageOverlay(bild, bounds).addTo(map);
+}
